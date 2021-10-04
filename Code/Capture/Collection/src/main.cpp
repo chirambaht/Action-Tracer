fbd701ef350ab14a480de02122192fdd3ee011ad
Humphrey Chiramba <chirambaht@gmail.com>
2021-10-04 16:58:08 +0200

    Lots of changes

--- Code/Capture/Collection/src/main.cpp
+++ Code/Capture/Collection/src/main.cpp
@@ -1,21 +1,42 @@
 // This is the main file that will be used to run the program for data
 // collection from the 3 IMU's and send them to the server as is necesarry.
-
+#include "main.h"
 /// Outline
 
+using namespace ActionTracer;
+
 /*
-    Initialise all the devices in the network. Store them in objects known as
-   ''(check main.h)
+    Initialise all the devices in the network. Store them in objects in main.h
 */
 
-void setup() {}
+void setup() {
+	communicator = new Packager( SERVER_IP, PORT ); // Initialize the communicator that will send data packets to the server
 
-void loop() {}
+	// The following are the sensors being used to obtain data from the body.
+	body_sensor[0] = new TracePoint( new MPU6050( MPU6050_ADDRESS_AD0_HIGH ), "Left Shoulder", 2, 1 );
+	body_sensor[1] = new TracePoint( new MPU6050( MPU6050_ADDRESS_AD0_HIGH ), "Left Forearm", 2, 1 );
+	body_sensor[2] = new TracePoint( new MPU6050( MPU6050_ADDRESS_AD0_HIGH ), "Left Hand", 2, 1 );
 
-int main(int argc, char const *argv[]) {
-  setup();
-  while (1) {
-    loop();
-  }
-  return 0;
+	debugPrint( "Looking at sensor 0, Left Shoulder" );
+	body_sensor[0].identify();
+}
+
+/*
+  Main work loop for all the code. This will always run every cycle.
+*/
+void loop() {
+	float data_package[N * 4];
+	for( size_t i = 0; i < N; i++ ) {
+		data_package[N * 4] = body_sensor[i].read_data();
+	}
+	communicator.send_packet( data_package, N * 4 );
+	usleep( LOOP_DELAY * 1000 );
+}
+
+int main( int argc, char const *argv[] ) {
+	setup();
+	while( 1 ) {
+		loop();
+	}
+	return 0;
 }
--- Code/Capture/Collection/src/main.h
+++ Code/Capture/Collection/src/main.h
@@ -1,6 +1,26 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#define SEND_TO_MOBILE
+
+#define SERVER_IP  "127.0.0.1" // Server IP where data will be sent to
+#define MOBILE_IP  "127.0.0.1" // Mobile IP where data will be sent to
+#define PORT	   9022		   // Main UDP data port for the data to be sent through
+#define N		   3		   // Number of points track on the body.
+#define LOOP_DELAY 1000		   // ms to wait per loop cycle
+
+#include "packager.h"
+#include "tracer_point.h"
+
+#include <unistd.h>
+
+using namespace ActionTracer;
+
+TracePoint body_sensor[N]; // These are the N sensors on the body.
+Packager   communicator;
+
+// Packager mobile = new Packager( MOBILE_IP, PORT );
+
 void setup();
 void loop();
 // namespace ActionTracer
