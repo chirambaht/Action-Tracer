a95a5d7c5996101ec28d5c646b413a1e78c1eb5f
Humphrey Chiramba <chirambaht@gmail.com>
2021-10-15 20:50:33 +0200

    Network logger added

--- Code/Capture/Collection/src/packager.h
+++ Code/Capture/Collection/src/packager.h
@@ -1,4 +1,5 @@
 #include <arpa/inet.h> //inet_addr
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <math.h>
@@ -10,12 +11,13 @@
 namespace ActionTracer {
 	class Packager {
 	  private:
-		std::string _dest;
-		int			_port;
-		int			_descriptor;
-		__uint32_t	_count;
-
-		std::string _float_to_string( float value, int prec );
+		std::string	  _dest;
+		int			  _port;
+		int			  _descriptor;
+		__uint32_t	  _count;
+		std::ofstream logger;
+		std::string	  _float_to_string( float value, int prec );
+		void		  _log( std::string data );
 
 	  public:
 		Packager();
