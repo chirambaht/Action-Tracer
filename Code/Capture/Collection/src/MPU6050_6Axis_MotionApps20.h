8b3c4d05c55aa8b87ee7126160c0b0f9320b5a73
Humphrey <chirambaht@gmail.com>
2021-10-23 14:04:00 +0200

    printer

--- Code/Capture/Collection/src/MPU6050_6Axis_MotionApps20.h
+++ Code/Capture/Collection/src/MPU6050_6Axis_MotionApps20.h
@@ -36,8 +36,8 @@ THE SOFTWARE.
 #define _MPU6050_6AXIS_MOTIONAPPS20_H_
 
 #include "I2Cdev.h"
+#include "debug_printer.h"
 #include "helper_3dmath.h"
-
 // MotionApps 2.0 DMP implementation, built using the MPU-6050EVB evaluation board
 #define MPU6050_INCLUDE_DMP_MOTIONAPPS20
 
