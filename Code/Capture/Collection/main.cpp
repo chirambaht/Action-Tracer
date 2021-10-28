afaff4bd3990661d206b2d39c5b9f2dde6105634
Humphrey <chirambaht@gmail.com>
2021-10-28 15:24:29 +0200

    base

--- Code/Capture/Collection/main.cpp
+++ Code/Capture/Collection/main.cpp
@@ -1,9 +1,9 @@
 // This is the main file that will be used to run the program for data
 // collection from the 3 IMU's and send them to the server as is necesarry.
-#include "debug_printer.h"
 #include "main.h"
 
-#include <cxxopts.hpp>
+#include "debug_printer.h"
+
 #include <dirent.h>
 #include <filesystem>
 #include <iostream>
@@ -27,7 +27,7 @@ cxxopts::Options options( "Action Tracer", "This program runs a given number of 
     Initialise all the devices in the network. Store them in objects in main.h
 */
 
-void setup( int debug_value = 0, std::string address = "127.0.0.1", ) {
+void setup( int debug_value = 0 ) {
 	wiringPiSetup();
 
 	communicator = new Packager( SERVER_IP, PORT ); // Initialize the communicator that will send data packets to the server
@@ -88,11 +88,11 @@ int main( int argc, char const *argv[] ) {
 	_sensors = result["tracepoints"].as<int>();
 
 	if( result.count( "file" ) ) {
-		std::string config_file = result["file"].as<std::string>();
 		// TODO: Read CSV file for parameters for each item
 	}
 
 	// TODO: Run a new setup method that accounts for debug, custom tps, files and addresses
+	setup( _debug );
 
 	while( 1 ) {
 		loop();
