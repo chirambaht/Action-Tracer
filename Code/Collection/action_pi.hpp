15a04f9a5ce8080999d9963fdec3e9d9c078f510
Humphrey Chiramba <chirambaht@gmail.com>
2022-04-12 20:56:14 +0200

    Updated tracer connector work

--- Code/Collection/action_pi.hpp
+++ Code/Collection/action_pi.hpp
@@ -1,5 +1,5 @@
 /**
- * This is used to define PI pins for the Action Tracer Suite of Applications.
+ * This is used to define PI pins for the Action Tracer Suite of Applications. Complies with the Action Connector.
  */
 #include <stddef.h>
 #ifndef ACTION_PI_H
@@ -10,25 +10,25 @@
  | AcT | wPi |   Name  | Mode | Physical | Mode | Name    | wPi | AcT |
  +-----+-----+---------+------+----++----+------+---------+-----+-----+
  |     |     |   3V3   |      |  1 || 2  |      |   5v    |     |     |
- |   2 |   8 |  Data   |      |  3 || 4  |      |   5v    |     |     |
- |   3 |   9 |  Clock  |      |  5 || 6  |      |   GND   |     |     |
- |   4 |   7 |         |      |  7 || 8  |      |         | 15  | 14  |
- |     |     |   GND   |      |  9 || 10 |      |         | 16  | 15  |
- |  17 |   0 |         |      | 11 || 12 |      |         | 1   | 18  |
- |  27 |   2 |         |      | 13 || 14 |      |   GND   |     |     |
- |  22 |   3 |         |      | 15 || 16 |      |         | 4   | 23  |
- |     |     |   3V3   |      | 17 || 18 |      |         | 5   | 24  |
- |  10 |  12 |         |      | 19 || 20 |      |   GND   |     |     |
- |   9 |  13 |         |      | 21 || 22 |      |         | 6   | 25  |
- |  11 |  14 |         |      | 23 || 24 |      |         | 10  | 8   |
- |     |     |   GND   |      | 25 || 26 |      |         | 11  | 7   |
- |   0 |  30 |         |      | 27 || 28 |      |         | 31  | 1   |
- |   5 |  21 |         |      | 29 || 30 |      |   GND   |     |     |
- |   6 |  22 |         |      | 31 || 32 |      |         | 26  | 12  |
- |  13 |  23 |         |      | 33 || 34 |      |   GND   |     |     |
- |  19 |  24 |         |      | 35 || 36 |      |         | 27  | 16  |
- |  26 |  25 |         |      | 37 || 38 |      |         | 28  | 20  |
- |     |     |   GND   |      | 39 || 40 |      |         | 29  | 21  |
+ |     |   8 |  Data   |      |  3 || 4  |      |   5v    |     |     |
+ |     |   9 |  Clock  |      |  5 || 6  |      |   GND   |     |     |
+ |   0 |   7 |         |      |  7 || 8  |      |         | 15  |     |
+ |     |     |   GND   |      |  9 || 10 |      |         | 16  |     |
+ |   1 |   0 |         |      | 11 || 12 |      |         | 1   |     |
+ |   2 |   2 |         |      | 13 || 14 |      |   GND   |     |     |
+ |   3 |   3 |         |      | 15 || 16 |      |         | 4   |     |
+ |     |     |   3V3   |      | 17 || 18 |      |         | 5   |     |
+ |   4 |  12 |         |      | 19 || 20 |      |   GND   |     |     |
+ |   5 |  13 |         |      | 21 || 22 |      |         | 6   |     |
+ |   6 |  14 |         |      | 23 || 24 |      |         | 10  |     |
+ |     |     |   GND   |      | 25 || 26 |      |         | 11  |     |
+ |   7 |  30 |         |      | 27 || 28 |      |         | 31  |     |
+ |   8 |  21 |         |      | 29 || 30 |      |   GND   |     |     |
+ |   9 |  22 |         |      | 31 || 32 |      |         | 26  |     |
+ |  10 |  23 |         |      | 33 || 34 |      |   GND   |     |     |
+ |  11 |  24 |         |      | 35 || 36 |      |         | 27  | INT |
+ |  12 |  25 |         |      | 37 || 38 |      |         | 28  |     |
+ |     |     |   GND   |      | 39 || 40 |      |         | 29  |     |
  +-----+-----+---------+------+----++----+------+---------+-----+-----+
  | AcT | wPi |   Name  | Mode | Physical | Mode | Name    | wPi | AcT |
  +-----+-----+---------+------+-Pi Zero--+------+---------+-----+-----+
@@ -40,6 +40,7 @@ namespace ActionTracer {
 	#define ACT_I2C_DATA  8
 	#define ACT_I2C_CLOCK 9
 
+	#define ACT_DEVICE_0  7
 	#define ACT_DEVICE_1  0
 	#define ACT_DEVICE_2  2
 	#define ACT_DEVICE_3  3
@@ -53,8 +54,10 @@ namespace ActionTracer {
 	#define ACT_DEVICE_11 24
 	#define ACT_DEVICE_12 25
 
+	#define ACT_DEVICE_0_INT 27
+
 	// An array with all the Pi pins available.
-	const int PI_ORDER[12] = { ACT_DEVICE_1, ACT_DEVICE_2, ACT_DEVICE_3, ACT_DEVICE_4, ACT_DEVICE_5, ACT_DEVICE_6, ACT_DEVICE_7, ACT_DEVICE_8, ACT_DEVICE_9, ACT_DEVICE_10, ACT_DEVICE_11, ACT_DEVICE_12 };
+	const int PI_ORDER[13] = { ACT_DEVICE_0, ACT_DEVICE_1, ACT_DEVICE_2, ACT_DEVICE_3, ACT_DEVICE_4, ACT_DEVICE_5, ACT_DEVICE_6, ACT_DEVICE_7, ACT_DEVICE_8, ACT_DEVICE_9, ACT_DEVICE_10, ACT_DEVICE_11, ACT_DEVICE_12 };
 
 	const size_t num_action_devices = 12;
 
