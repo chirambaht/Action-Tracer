73f6a8bf445e918ab6494a988e24fa5b1f57a120
Humphrey <chirambaht@gmail.com>
2022-05-04 13:48:19 +0200

    add

--- Code/Collection/test.cpp
+++ Code/Collection/test.cpp
@@ -1,14 +1,15 @@
+
+
 #include "MAX30102.h"
 
+#include <chrono>
 #include <cstdio>
-#ifdef ON_PI
-	#include <wiringPi.h>
-#endif
+
+auto start = std::chrono::steady_clock::now();
 
 int main( int argc, char const *argv[] ) {
-#ifdef ON_PI
-	wiringPiSetupSys();
-#endif
+	start = std::chrono::steady_clock::now();
+
 	printf( "This is the MAX30102%sTest\n", " " );
 
 	MAX30102 *dev = new MAX30102();
@@ -20,4 +21,17 @@ int main( int argc, char const *argv[] ) {
 	printf( "Got this part ID: %x\n", id );
 
 	return 0;
+}
+
+unsigned int millis( void ) {
+	auto end = std::chrono::steady_clock::now();
+	return std::chrono::duration_cast<std::chrono::milliseconds>( end - start ).count();
+}
+
+void delay( unsigned int duration ) {
+	auto t	 = std::chrono::steady_clock::now();
+	auto end = std::chrono::steady_clock::now();
+	do {
+		end = std::chrono::steady_clock::now();
+	} while( std::chrono::duration_cast<std::chrono::milliseconds>( end - t ).count() < duration );
 }
