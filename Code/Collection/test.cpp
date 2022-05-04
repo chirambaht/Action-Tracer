824028742e7a6351de946618b61a4c8001aa9e7e
Humphrey <chirambaht@gmail.com>
2022-05-04 13:33:51 +0200

    Timing

--- Code/Collection/test.cpp
+++ Code/Collection/test.cpp
@@ -1,8 +1,14 @@
 #include "MAX30102.h"
 
 #include <cstdio>
+#ifdef ON_PI
+	#include <wiringPi.h>
+#endif
 
 int main( int argc, char const *argv[] ) {
+#ifdef ON_PI
+	wiringPiSetupSys();
+#endif
 	printf( "This is the MAX30102%sTest\n", " " );
 
 	MAX30102 *dev = new MAX30102();
