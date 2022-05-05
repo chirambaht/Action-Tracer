5d6bcedc861983c9dea2c5bced19f5f31c742900
Humphrey <chirambaht@gmail.com>
2022-05-05 16:27:32 +0200

    done

--- Code/Collection/pyth.py
+++ Code/Collection/pyth.py
@@ -10,17 +10,26 @@ bus = SMBus(channel)
 def quit():
     exit(1)
 
+def get_bin(tt):
+    
+    temp = bin(tt)[2:]
+    while len(temp) < 8:
+        temp = "0" + temp
+    out = temp[:4] + " " + temp[4:]
+    
+
 def read_reg():
     r = eval("0x" + input("Which register: 0x"))
     b = bus.read_byte_data(address, r)
-    print(f"{hex(r)} - {hex(b)} | {bin(b)} | {b}")
+    
+    print(f"{hex(r)} - {hex(b)} | {get_bin(b)} | {b}")
     print()
 
 def write_reg():
     r = eval("0x" + input("Which register: 0x"))
     d = eval("0x" + input("Data: 0x"))
     b = bus.write_byte_data(address, r, d)
-    print(f"{hex(r)} - {hex(d)} | {bin(d)} | {d}")
+    print(f"{hex(r)} - {hex(d)} | {get_bin(d)} | {d}")
     print()
 
 while True:
