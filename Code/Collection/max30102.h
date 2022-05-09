7ca69de10c141f42632f160194a2b384478116b9
Humphrey <chirambaht@gmail.com>
2022-05-09 14:41:26 +0200

    small

--- Code/Collection/max30102.h
+++ Code/Collection/max30102.h
@@ -92,16 +92,10 @@
 
 #include <cstdint>
 
-uint8_t data_buffer[8192];
+uint8_t data_buffer[1024];
 
 bool maxim_max30102_init();
-#if defined( ARDUINO_AVR_UNO )
-// Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
-// To solve this problem, 16-bit MSB of the sampled data will be truncated.  Samples become 16-bit data.
-bool maxim_max30102_read_fifo( uint16_t *pun_red_led, uint16_t *pun_ir_led );
-#else
 bool maxim_max30102_read_fifo( uint32_t *pun_red_led, uint32_t *pun_ir_led );
-#endif
 bool maxim_max30102_write_reg( uint8_t uch_addr, uint8_t uch_data );
 bool maxim_max30102_read_reg( uint8_t uch_addr, uint8_t *puch_data );
 bool maxim_max30102_reset( void );
