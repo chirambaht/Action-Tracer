47832fb6d561c61defaf3628d7f20b4091527313
Humphrey <chirambaht@gmail.com>
2022-07-01 16:02:36 +0200

    new mode added

--- Collection/main.cpp
+++ Collection/main.cpp
@@ -109,8 +109,13 @@ void setup() {
 			name = "IMU_3";
 		}
 
-		printf( "New device initialising on WiringPi pin %d aka ACT_%d\n", get_pi_location( i + 1 ), i + 1 );
+		#ifdef SINGLE_ACT_0
+		// Init all the devices as the IMU on the Pi Connector
+		printf( "New device initialising on WiringPi pin %d aka ACT_%d\n", get_pi_location( 0 ), i + 1 );
+		body_sensor[i] = new TracePoint( i, get_pi_location( 0 ) );
+		#else 
 		body_sensor[i] = new TracePoint( i, get_pi_location( i + 1 ) ); // offset by 1 is to ensure we are starting at ACT_DEVICE_1
+		#endif
 	}
 }
 
