d7533c45be674c7a340f7e677c158ebf0d12d288
Humphrey <chirambaht@gmail.com>
2022-07-11 20:39:48 +0200

    Lots of minor changes

--- assist_runners/assist_network.py
+++ assist_runners/assist_network.py
@@ -3,7 +3,7 @@ import socket
 from datetime import datetime
 import numpy as np
 import time
-import pandas as pd
+# import pandas as pd
 
 
 col = ['Time', 'Count', 'Devices']
@@ -11,13 +11,9 @@ for f in range(3):
     for k in ["W", "I", "J", "K"]:
         col.append(k + str(f+1) )
 
-def smooth_bin_string(b_string):
-    t = []
-    for i in b_string:
-        t.append(hex(i))
-    return b_string
 
-def bin_to_good(data_in, bytes_per_data_point):
+# Get the data from the packet as an array of integers
+def bin_to_good(data_in, bytes_per_data_point, convert_to_float=False):
     pre_work = []
     data_packet = []
     
@@ -25,33 +21,23 @@ def bin_to_good(data_in, bytes_per_data_point):
         pre_work.append(data_in[i*bytes_per_data_point:(i*bytes_per_data_point)+bytes_per_data_point])
     
     for i in pre_work:
-        data_packet.append(int.from_bytes(i, byteorder="little"))
+        if convert_to_float:
+            data_packet.append(int.from_bytes(i, byteorder="little", signed=True) / 10000)
+            
+        else:
+            data_packet.append(int.from_bytes(i, byteorder="little", signed=True) )
 
     return data_packet
 
 def bin_to_readable_data(data_in, bytes_per_data_point):
-    pre_work = bin_to_good(data_in, bytes_per_data_point)
+    pre_work = bin_to_good(data_in, bytes_per_data_point, False)
     buffer_string_thing = ""
 
-    print(pre_work)
+    # print(pre_work)
 
     for i in pre_work:
-        buffer_string_thing += str(int.from_bytes(i, byteorder="little")) + " "
+        buffer_string_thing += str(i) + " "
     return buffer_string_thing
-
-
-def swapper(bin_string_array):
-    r = len(bin_string_array)
-    final_array = []
-    #print half the array length
-    print("Half:",r//2)
-
-    for i in range(r//2):
-        ss = "0x%2x%2x" % (bin_string_array[(i*2)+1], bin_string_array[i*2])
-        si = int(ss.replace(" ", "0"), 16)
-        final_array.append(np.int16(si))
-
-    return final_array
 
 
 def millis():
@@ -59,7 +45,7 @@ def millis():
     return t.second + t.n
 
 
-ss = time.clock_gettime(1)
+ss = time.time()
 running = False
 
 
@@ -67,7 +53,7 @@ def check_time():
     global running
     global ss
     if running == False:
-        ss = time.clock_gettime(1)
+        ss = time.time()
         running = True
     return ss
 
@@ -124,8 +110,8 @@ while (True):
     s.connect((HOST, PORT))
 
     now = datetime.now()
-    start_time = time.clock_gettime(1)
-    end_time = time.clock_gettime(1)
+    start_time = time.time()
+    end_time = time.time()
     current_time = now.strftime("%Y%m%d-%H%M%S")
 
     logger = open(current_time+".act", "w")
@@ -138,29 +124,28 @@ while (True):
         start_time = check_time()
 
         if not data:
-            end_time = time.clock_gettime(1)
+            end_time = time.time()
             running = False
             break
         
         print("Received data length: %d" % (len(data)))
-        # print data as hex string
 
         header = data[:12]
-        device_data_1 = data[12:88]
-        device_data_2 = data[88:164]
-        device_data_3 = data[164:240]
 
-        print(data)
-        print("\nHeader:",bin_to_readable_data(header, 4))
-        print("Device 1:",bin_to_readable_data(device_data_1, 2))
-        print("Device 2:",bin_to_readable_data(device_data_2, 2))
-        print("Device 3:",bin_to_readable_data(device_data_3, 2))
-
+        rest_of_data = data[12:240]
+        print("Received data: %s" % (bin_to_readable_data(data, 4)))
         # correct the order that data comes in (saved in t)
-        t = swapper(data)
+        t = bin_to_good(rest_of_data, 4, True)
+        h_data = bin_to_good(header, 4)
+
+        print(f"\nTime: {h_data[0]}, Count: {h_data[1]}, Devices: {h_data[2]}")
+
+        # print t data in groups of 19.
+        for i in range(len(t)//19):
+            print(f"Device {i+1}:{t[i*19:(i*19)+19]}")
        
-        cs.append(clean_arr(t, 10000, 3))
-        c = t[1]
+        # cs.appensd(clean_arr(t, 10000, 3))
+        c = h_data[1]
     logger.close()
 
     print("Last log to %s.act" % (current_time))
@@ -169,8 +154,8 @@ while (True):
     print(
         f"Average packets received was {c/ft:.2f}/s. \n{c} packets were received in {ft:.3f}s.\nTotal packets Recevied: {len(cs)}/{c} {round((len(cs)/c)*100,2)}%")
 
-    df = pd.DataFrame(cs, columns=col)
-    df.to_csv(f"{current_time}.csv", index=False)
+    # df = pd.DataFrame(cs, columns=col)
+    # df.to_csv(f"{current_time}.csv", index=False)
     tt += 1
 
 
