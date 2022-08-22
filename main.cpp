a8cc1e42716a76b919caf0705599e008d07c5b3e
Humphrey <chirambaht@gmail.com>
2022-08-22 18:17:42 +0200

    Show that body!

--- main.cpp
+++ main.cpp
@@ -49,6 +49,7 @@ void setup() {
 	main_dev->set_sample_rate( 2 );
 	printf( "All set to go \n" );
 	main_dev->initialize( 2 );
+	main_dev->show_body();
 	printf( "Initialised\n" );
 	main_dev->start();
 }
