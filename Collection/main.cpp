b6de8104139c8a368dfa361c5936741ac345bfb8
Humphrey <chirambaht@gmail.com>
2022-12-07 13:08:04 +0200

    Unthreaded test

--- Collection/main.cpp
+++ Collection/main.cpp
@@ -7,117 +7,127 @@
 #include "debug_printer.h"
 
 #ifdef TAKE_ARGUMENTS
-	#include <cxxopts.hpp>
-	#include <string>
+#include <cxxopts.hpp>
+#include <string>
 #endif
 
 #include <signal.h>
 #include <stdlib.h>
 
 #ifdef ON_PI
-	#include <wiringPi.h>
+#include <wiringPi.h>
 #endif
 
 using namespace ActionTracer;
 
 #ifdef TAKE_ARGUMENTS
-cxxopts::Options options( "Action Tracer", "This program runs a given number of MPU6050 IMU's and sends the data packets via UDP." );
+cxxopts::Options options("Action Tracer",
+                         "This program runs a given number of MPU6050 IMU's "
+                         "and sends the data packets via UDP.");
 #endif
 
 #ifdef ON_PI
-PI_THREAD( network_watcher ) {
-	// void n() {
+PI_THREAD(network_watcher) {
+  // void n() {
 
-	printf( "Network Management Thread starting...\n" );
-	communicator = new Packager( PORT ); // Initialize the communicator that will send data packets to the server
-	// communicator->_number_of_devices = _sensors;
-	communicator->socket_setup();
+  printf("Network Management Thread starting...\n");
+  communicator = new Packager(PORT); // Initialize the communicator that will
+                                     // send data packets to the server
+  // communicator->_number_of_devices = _sensors;
+  communicator->socket_setup();
 
-	// while( communicator->_clients_connected() < 1 ) {
-	// 	printf( "clients: %d\n", communicator->_clients_connected() );
-	// }
-	// printf( "At least 1 client connected: %d\n", communicator->_clients_connected() );
+  // while( communicator->_clients_connected() < 1 ) {
+  // 	printf( "clients: %d\n", communicator->_clients_connected() );
+  // }
+  // printf( "At least 1 client connected: %d\n",
+  // communicator->_clients_connected() );
 
-	for( ;; ) {
-		if( send_ready == false ) {
-			communicator->run_socket_manager();
-		}
-	}
+  for (;;) {
+    if (send_ready == false) {
+      communicator->run_socket_manager();
+    }
+  }
 }
 
-PI_THREAD( network_sender ) {
-	printf( "Sending thread starting...\n" );
-	for( ;; ) {
-		while( !send_ready ) {
-			continue;
-		}
-		piLock( 1 );
+PI_THREAD(network_sender) {
+  printf("Sending thread starting...\n");
+  for (;;) {
+    while (1 || !send_ready) {
+      continue;
+    }
+    piLock(1);
 
-		// Send packet
-		communicator->send_to_connected_devices();
-		send_ready = false;
+    // Send packet
+    communicator->send_to_connected_devices();
+    send_ready = false;
 
-		piUnlock( 1 );
-	}
+    piUnlock(1);
+  }
 }
 #endif
 
 void turn_off_all_devices() {
-	for( size_t i = 0; i < _sensors; i++ ) {
-		body_sensor[i]->turn_off();
-	}
+  for (size_t i = 0; i < _sensors; i++) {
+    body_sensor[i]->turn_off();
+  }
 }
 
 /**
- * @brief Initialise all the devices in the network. Store them in objects in main.h
+ * @brief Initialise all the devices in the network. Store them in objects in
+ * main.h
  * @return 0 if success
  */
 void setup() {
 #ifdef ON_PI
-	wiringPiSetup();
+  wiringPiSetup();
 #endif
 
-	struct sigaction sigIntHandler;
+  struct sigaction sigIntHandler;
 
-	sigIntHandler.sa_handler = exit_handler;
-	sigemptyset( &sigIntHandler.sa_mask );
-	sigIntHandler.sa_flags = 0;
+  sigIntHandler.sa_handler = exit_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
 
-	sigaction( SIGINT, &sigIntHandler, NULL );
+  sigaction(SIGINT, &sigIntHandler, NULL);
 
-	// Create thread to work on networking with the packager
+  // Create thread to work on networking with the packager
 
 #ifdef ON_PI
-	// Will break here if it doesn't find a TCP socket available
-	if( piThreadCreate( network_watcher ) != 0 ) {
-		printf( "Failed to create network worker thread\n" );
-		exit( EXIT_FAILURE );
-	}
-	if( piThreadCreate( network_sender ) != 0 ) {
-		printf( "Failed to create network worker thread\n" );
-		exit( EXIT_FAILURE );
-	}
+  // Will break here if it doesn't find a TCP socket available
+  if (piThreadCreate(network_watcher) != 0) {
+    printf("Failed to create network worker thread\n");
+    exit(EXIT_FAILURE);
+  }
+  if (piThreadCreate(network_sender) != 0) {
+    printf("Failed to create network worker thread\n");
+    exit(EXIT_FAILURE);
+  }
 #endif
-	for( size_t i = 0; i < _sensors; i++ ) {
+  for (size_t i = 0; i < _sensors; i++) {
 #ifdef SINGLE_ACT_0
-		// Init all the devices as the IMU on the Pi Connector
-		printf( "New device initialising on WiringPi pin %d aka ACT_%d\n", get_pi_location( 0 ), i + 1 );
-		body_sensor[i] = new TracePoint( i, get_pi_location( 0 ) );
+    // Init all the devices as the IMU on the Pi Connector
+    printf("New device initialising on WiringPi pin %d aka ACT_%d\n",
+           get_pi_location(0), i + 1);
+    body_sensor[i] = new TracePoint(i, get_pi_location(0));
 #else
-		printf( "New device initialising on WiringPi pin %d aka ACT_%d\n", get_pi_location( i + 1 ), i + 1 );
-		body_sensor[i] = new TracePoint( i, get_pi_location( i + 1 ) ); // offset by 1 is to ensure we are starting at ACT_DEVICE_1
+    printf("New device initialising on WiringPi pin %d aka ACT_%d\n",
+           get_pi_location(i + 1), i + 1);
+    body_sensor[i] = new TracePoint(
+        i,
+        get_pi_location(
+            i + 1)); // offset by 1 is to ensure we are starting at ACT_DEVICE_1
 #endif
-	}
+  }
 }
 
-void exit_handler( int s ) {
-	printf( "\nTurning off all devices...\n" );
-	turn_off_all_devices();
-	printf( "Cleaning up the communicator\n" );
-	communicator->dump_vars();
-	printf( "Closing socket\n" );
-	communicator->close_socket();
-	exit( 1 );
+void exit_handler(int s) {
+  printf("\nTurning off all devices...\n");
+  turn_off_all_devices();
+  printf("Cleaning up the communicator\n");
+  communicator->dump_vars();
+  printf("Closing socket\n");
+  communicator->close_socket();
+  exit(1);
 }
 
 /**
@@ -125,71 +135,68 @@ void exit_handler( int s ) {
  * @return Nothing
  */
 void loop() {
-	float *body;
-#ifdef ON_PI
-	loop_start = millis();
-	while( millis() - loop_start < ( WAIT_TIME / 2 ) ) {
-		continue;
-	}
-	loop_start = millis();
-#endif
-	for( size_t i = 0; i < _sensors; i++ ) {
-		body = body_sensor[i]->read_data( 1 );
-		for( size_t j = 0; j < 4; j++ ) {
-			data_package[j] = *body;
-			body++;
-		}
-		communicator->load_packet( data_package, 4 );
-	}
-#ifdef ON_PI
-	piLock( 1 );
-	send_ready = true;
+  float *body;
 
-	while( millis() - loop_start < ( WAIT_TIME / 2 ) ) {
-		continue;
-	}
-	piUnlock( 1 );
+  for (size_t i = 0; i < _sensors; i++) {
+    body = body_sensor[i]->read_data(1);
+    for (size_t j = 0; j < 4; j++) {
+      data_package[j] = *body;
+      body++;
+    }
+    communicator->load_packet(data_package, 4);
+  }
+  communicator->send_to_connected_devices();
+#ifdef ON_PI
+  piLock(1);
+  send_ready = true;
+  piUnlock(1);
 #endif
 }
 
 /**
- * @brief Main method that schedules the main tasks before and after data collection.
+ * @brief Main method that schedules the main tasks before and after data
+ * collection.
  *
  * @param argc
  * @param argv
  * @return int
  */
-int main( int argc, char const *argv[] ) {
+int main(int argc, char const *argv[]) {
 #ifdef TAKE_ARGUMENTS
-	options.add_options()( "f,file", "Define variables using a file. If a file is given, all other given parameters will be overwritten.", cxxopts::value<std::string>()->default_value( "" ) );
-	options.add_options()( "h,help", "Print usage" );
-	options.add_options()( "t,tracepoints", "Number of body devices being used on the body", cxxopts::value<int>()->default_value( "0" ) );
+  options.add_options()("f,file",
+                        "Define variables using a file. If a file is given, "
+                        "all other given parameters will be overwritten.",
+                        cxxopts::value<std::string>()->default_value(""));
+  options.add_options()("h,help", "Print usage");
+  options.add_options()("t,tracepoints",
+                        "Number of body devices being used on the body",
+                        cxxopts::value<int>()->default_value("0"));
 
-	auto result = options.parse( argc, argv );
+  auto result = options.parse(argc, argv);
 
-	if( result.count( "help" ) ) {
-		printf( "%s", options.help().c_str() );
-		exit( 0 );
-	}
+  if (result.count("help")) {
+    printf("%s", options.help().c_str());
+    exit(0);
+  }
 
-	_sensors = result["tracepoints"].as<int>();
+  _sensors = result["tracepoints"].as<int>();
 
-	if( result.count( "file" ) ) {
-		// TODO: Read CSV file for parameters for each item
-	}
+  if (result.count("file")) {
+    // TODO: Read CSV file for parameters for each item
+  }
 #else
 
-	_sensors = 3;
-	printf( "Devices connected: %d\n", _sensors );
+  _sensors = 3;
+  printf("Devices connected: %d\n", _sensors);
 #endif
 
-	// TODO: Run a new setup method that accounts for debug, custom tps, files and
-	printf( "Running basic setup routine\n" );
-	setup();
+  // TODO: Run a new setup method that accounts for debug, custom tps, files and
+  printf("Running basic setup routine\n");
+  setup();
 
-	printf( "\nSetup Complete! Running now\n\n" );
-	while( 1 ) {
-		loop();
-	}
-	return 0;
+  printf("\nSetup Complete! Running now\n\n");
+  while (1) {
+    loop();
+  }
+  return 0;
 }
